72df82dd1894bbe5ab23502b1876a2f922f67748
aryansri07 <aryan.srivastava.met19@itbhu.ac.in>
2025-03-26 19:18:41 +0530

    navbar added

--- Front-end Chatbot/src/App.js
+++ Front-end Chatbot/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Chatbot from "./components/Chatbot"; // Existing component
 import ChartsAndTable from "./components/ChartsAndTable"; // New component
 import "./App.css"; // Optional: Add some basic styling
-
+import Navbar from "./components/NavbarContainer"
 function App() {
 
   return (
     <Router>
-        <main>
+      <Navbar /> {/* Add the Navbar here */}
+      <main style={{ marginTop: '60px' }}> 
+        {/* <main]> */}
           <Routes>
             <Route path="/" element={<Chatbot />} />
           </Routes>
